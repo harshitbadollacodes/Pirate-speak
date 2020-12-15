f93910748524f1cd951cc7513ef0849fc85f8c8f
Harshit Badolla <harshitbadolla5@gmail.com>
2020-12-15 10:59:40 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const userInput = document.querySelector('.input-text');
+const button = document.querySelector('.btn');
+const output = document.querySelector('.output');
+
+const url = `https://api.funtranslations.com/translate/pirate.json`;
+
+function convert() {
+    fetch(`${url}?text=${userInput.value}`)
+    .then(repsonse => repsonse.json())
+    .then(json => {
+        output.innerText = json.contents.translated;
+    })
+    .catch(() => alert('Some error occured. Please try after sometime'));
+};
+
+button.addEventListener('click', () => {
+    convert();
+});
